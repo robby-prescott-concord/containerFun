13eea0d794125d7f34e2d13e51e9e2b163854066
Robby Prescott <robby.prescott@concordusa.com>
2018-07-19 13:30:30 -0500

    second commit

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -3,6 +3,7 @@ package hello;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
@@ -14,8 +15,15 @@ public class Application {
         return "Hello Docker World";
     }
 
+    @RequestMapping(value = "/id")
+    String getIdByValue(@RequestParam("id") int personId){
+        System.out.println("ID squared is "+ (personId * personId));
+        return "Get ID from query string of URL with value element";
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
 
 }
+
